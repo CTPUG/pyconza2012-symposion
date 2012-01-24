ae605ba122bd875dc871d47087c63a8b30324ab4
Luke Hatcher <lukeman@gmail.com>
2012-01-24 01:51:07 -0500

    add more fields to the admin list

--- symposion_project/apps/menu/admin.py
+++ symposion_project/apps/menu/admin.py
@@ -5,4 +5,8 @@ from mptt.admin import MPTTModelAdmin
 from menu.models import MenuItem
 
 
-admin.site.register(MenuItem, MPTTModelAdmin)
+class MenuItemAdmin(MPTTModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "login_required", "published",)
+
+admin.site.register(MenuItem, MenuItemAdmin)
